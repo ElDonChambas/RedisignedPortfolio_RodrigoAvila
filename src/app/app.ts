1e36ad18c6beecd53bca4ee7560c4cec8f361592
ElDonChambas <rodrigo.salvador.avila.aguilar@gmail.com>
2026-06-08 21:52:31 -0600

    Primer commit: Configuración de Tailwind y sección Hero Banner completada

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroBanner } from './components/01-hero-banner/hero-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroBanner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/03-trabajos/trabajos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trabajos',
+  imports: [],
+  templateUrl: './trabajos.html',
+  styleUrl: './trabajos.css',
+})
+export class Trabajos {
+
+}
